603f2b98f591ec66e36a829ebeee2918896644d1
PRATICK\User <daspratick60@gmail.com>
2025-01-07 21:15:00 +0530

    New Changes, New Class Added

--- src/Common_Math.java
+++ src/Common_Math.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Common_Math {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(12-2);
+    }
 }
---
+++ src/Testing_Github.java
@@ -0,0 +1,6 @@
+public class Testing_Github
+{
+    public static void main(String[] args) {
+        System.out.println(500+500);
+    }
+}
