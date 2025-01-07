090bbb1b565a093a2eca9f5ce12afee1b24254da
PRATICK\User <daspratick60@gmail.com>
2025-01-07 21:15:00 +0530

    Print_f class added and new changes added.

---
+++ src/Print_f.java
@@ -0,0 +1,13 @@
+public class Print_f {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.printf("variable is %d",a);
+
+        byte b = 50;
+
+        //System.out.println();
+
+        System.out.printf(" variable is %d",a);
+    }
+}
---
+++ src/Printf_Task.java
@@ -0,0 +1,16 @@
+public class Printf_Task {
+    public static void main(String[] args) {
+        int num=9;
+        System.out.printf("9*1=%d",num*1);
+        System.out.printf("9*2=%d",num*2);
+        System.out.printf("9*3=%d",num*3);
+        System.out.printf("9*4=%d",num*4);
+        System.out.printf("9*5=%d",num*5);
+        System.out.printf("9*6=%d",num*6);
+        System.out.printf("9*7=%d",num*7);
+        System.out.printf("9*8=%d",num*8);
+        System.out.printf("9*9=%d",num*9);
+        System.out.printf("9*10=%d",num*10);
+
+    }
+}
